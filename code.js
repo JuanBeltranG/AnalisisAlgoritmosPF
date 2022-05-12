7f722aedc6b87ed10171e43523035c8c090db53f
Juan Beltran Garcia <jbeltrang1603@alumno.ipn.mx>
2022-05-12 01:05:35 -0500

    generando los primeros nodos con caracter y su ocirrencia

--- code.js
+++ code.js
@@ -9,5 +9,51 @@ var cy = cytoscape({
           source: 'a',
           target: 'b'
         }
-      }]
+      }],
+      style: [
+        {
+          selector: 'node',
+          css: {
+            'content': 'data(id)',
+            'text-valign': 'center',
+            'text-halign': 'center'
+          }
+        }]
   });
+
+  function cuentaOcurrencias() {
+
+    const input = document.getElementById("texto");
+    const texto = input.value;
+
+    var ocurrencias = new Map();
+
+    for (var i = 0; i < texto.length; i++) {
+
+        currChar = texto.charAt(i);
+
+        if (ocurrencias.has(currChar)) {
+            //El caracter ya ha sido guardado en el map
+            ocurrencias.set(currChar, ocurrencias.get(currChar) + 1);
+
+        } else {
+            //No hay ningun registro de ese caracter en el map
+            ocurrencias.set(currChar, 1);
+        }
+
+    }
+
+    //console.log(ocurrencias);
+
+
+  for (let [key, value] of ocurrencias) {
+   
+    cy.add({
+      data: { id: key + "/" + value }
+    });
+
+  }
+
+
+
+}
