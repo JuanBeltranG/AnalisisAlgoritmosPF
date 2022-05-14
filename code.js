f44f87532bc509c33bd30334c3251401506915c7
nero-ventus <devill_k.evin@hotmail.com>
2022-05-14 12:17:29 -0500

    Animacion por pasos hacia adelante

--- code.js
+++ code.js
@@ -90,6 +90,78 @@ class MinHeap {
   }
 }
 
+// Variables Globales
+
+var cy;
+var layout;
+var min_heap;
+let options = {
+  name: 'breadthfirst',
+
+  fit: true, // whether to fit the viewport to the graph
+  directed: true, // whether the tree is directed downwards (or edges can point in any direction if false)
+  padding: 30, // padding on fit
+  circle: false, // put depths in concentric circles if true, put depths top down if false
+  grid: false, // whether to create an even grid into which the DAG is placed (circle:false only)
+  spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+  boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+  nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
+  roots: undefined, // the roots of the trees
+  maximal: false, // whether to shift nodes down their natural BFS depths in order to avoid upwards edges (DAGS only)
+  depthSort: undefined, // a sorting function to order nodes at equal depth. e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+  animate: true, // whether to transition the node positions
+  animationDuration: 500, // duration of animation in ms if enabled
+  animationEasing: undefined, // easing of animation if enabled,
+  animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+  ready: undefined, // callback on layoutready
+  stop: undefined, // callback on layoutstop
+  transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+};
+
+// Fin de variables globales
+
+// Cosas Kevin
+
+function nextMove(){
+  if(min_heap.heap.length > 2){
+    var current_node = new TreeNode();
+    current_node.left = min_heap.remove();
+    current_node.right = min_heap.remove();
+
+    current_node.key = current_node.left.key + "-" + current_node.right.key;
+    current_node.val = current_node.left.val + current_node.right.val;
+
+    min_heap.insert(current_node);
+
+    cy.add({
+      data: { id: current_node.key + "/" + current_node.val }
+    });
+
+    cy.add({
+      data: {
+        id: current_node.key + current_node.left.key,
+        source: current_node.key + "/" + current_node.val,
+        target: current_node.left.key + "/" + current_node.left.val
+      }
+    });
+    cy.add({
+      data: {
+        id: current_node.key + current_node.right.key,
+        source: current_node.key + "/" + current_node.val,
+        target: current_node.right.key + "/" + current_node.right.val
+      }
+    });
+    
+    layout.stop();
+    layout = cy.layout( options );
+    layout.run();
+
+  }
+}
+
+// Fin cosas Kevin
+
 function generaArbol() {
 
   const input = document.getElementById("texto");
@@ -121,7 +193,7 @@ function generaArbol() {
 
 
   //Aqui declaramos nuestro espacio donde el grafo sera dibujado asi como las proiedades de estilo
-  var cy = cytoscape({
+  cy = cytoscape({
     container: document.getElementById('cy'),
     /*elements: [
       { data: { id: 'a' } },
@@ -146,34 +218,9 @@ function generaArbol() {
 
   // Cosas Kevin
 
-  let options = {
-    name: 'breadthfirst',
-  
-    fit: true, // whether to fit the viewport to the graph
-    directed: true, // whether the tree is directed downwards (or edges can point in any direction if false)
-    padding: 30, // padding on fit
-    circle: false, // put depths in concentric circles if true, put depths top down if false
-    grid: false, // whether to create an even grid into which the DAG is placed (circle:false only)
-    spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
-    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-    avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
-    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
-    roots: undefined, // the roots of the trees
-    maximal: false, // whether to shift nodes down their natural BFS depths in order to avoid upwards edges (DAGS only)
-    depthSort: undefined, // a sorting function to order nodes at equal depth. e.g. function(a, b){ return a.data('weight') - b.data('weight') }
-    animate: true, // whether to transition the node positions
-    animationDuration: 500, // duration of animation in ms if enabled
-    animationEasing: undefined, // easing of animation if enabled,
-    animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-    ready: undefined, // callback on layoutready
-    stop: undefined, // callback on layoutstop
-    transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
-  };
+  layout = cy.layout( options );
 
-  var layout = cy.layout( options );
-
-  var min_heap = new MinHeap();
-  var number_nodes = 0;
+  min_heap = new MinHeap();
 
   for (let [key, value] of ocurrencias){
     var aux_mode = new TreeNode();
@@ -185,44 +232,6 @@ function generaArbol() {
       data: { id: key + "/" + value }
     });
 
-    layout.stop();
-    layout = cy.layout( options );
-    layout.run();
-
-    number_nodes++;
-  }
-
-  while(min_heap.heap.length > 2){
-    var current_node = new TreeNode();
-    current_node.left = min_heap.remove();
-    current_node.right = min_heap.remove();
-
-    current_node.key = current_node.left.key + "-" + current_node.right.key;
-    current_node.val = current_node.left.val + current_node.right.val;
-
-    min_heap.insert(current_node);
-
-    number_nodes++;
-
-    cy.add({
-      data: { id: current_node.key + "/" + current_node.val }
-    });
-
-    cy.add({
-      data: {
-        id: current_node.key + current_node.left.key,
-        source: current_node.key + "/" + current_node.val,
-        target: current_node.left.key + "/" + current_node.left.val
-      }
-    });
-    cy.add({
-      data: {
-        id: current_node.key + current_node.right.key,
-        source: current_node.key + "/" + current_node.val,
-        target: current_node.right.key + "/" + current_node.right.val
-      }
-    });
-    
     layout.stop();
     layout = cy.layout( options );
     layout.run();
